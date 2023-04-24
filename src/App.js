ccb60c396b956994f1571ec9d722197048b1f4a7
Paulo Silvério <paulohmsilverio@gmail.com>
2023-04-23 22:41:28 -0300

    Atividade1
    Letreiro e Relogio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Relogio from "./Components/Relogio";
+import Letreiro from "./Components/Letreiro";
+
+function App() {
+  return (
+    <>
+      <Relogio />
+      <Letreiro />
+    </>
+  );
+}
+
+export default App;
